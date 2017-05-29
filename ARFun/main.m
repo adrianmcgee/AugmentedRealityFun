99590de5fd77b61027e4fd172940b179aaccc40a
Adrian McGee <adrianm@specialistapps.com>
2017-05-29 22:04:13 +1000

    Initial Commit

---
+++ ARFun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ARFun
+//
+//  Created by Adrian McGee on 29/5/17.
+//  Copyright © 2017 Adrian McGee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
